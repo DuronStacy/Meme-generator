e6c88a745fa010673e756dcaf6ac65f8c5833258
rasha84 <rasha.slman84@gmail.com>
2018-06-13 14:20:10 +0200

    slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,8 +37,11 @@ function updateFirst(event) {
   if (p) {
     p.style.color = event.target.value;
   }
-}function updateAll(event) {
+}
+function updateAll(event) {
   document.querySelectorAll("p").forEach(function(p) {
     p.style.color = event.target.value;
   });
-} 
+}
+
+
